29bcaaa0cabfe8f4364343808c31e67c15bc780c
Michał Sapalski <sapalskimichal@gmail.com>
2013-03-11 12:50:28 +0100

    Test for basic generics support.

---
+++ idea/testData/quickfix/override/nothingToOverride/beforeChangeParameterTypeGenerics.kt
@@ -0,0 +1,8 @@
+// "Change method signature to 'override fun <T>bar(y: T, x: Int)'" "true"
+open class A {
+    open fun <T>bar(a: T, b: Int) {}
+}
+
+class B : A(){
+    <caret>override fun <T>bar(x: Int, y: T) {}
+}
